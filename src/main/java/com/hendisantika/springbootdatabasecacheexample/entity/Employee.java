26c039d38e3bbd94beee7422f53454fbd388fab5
Hendi Santika <hendisantika@gmail.com>
2018-09-26 06:50:08 +0700

    #7 - Add Employee Entity

--- src/main/java/com/hendisantika/springbootdatabasecacheexample/entity/Employee.java
+++ src/main/java/com/hendisantika/springbootdatabasecacheexample/entity/Employee.java
@@ -1,5 +1,11 @@
 package com.hendisantika.springbootdatabasecacheexample.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-database-cache-example
@@ -10,5 +16,22 @@ package com.hendisantika.springbootdatabasecacheexample.entity;
  * Time: 06.48
  * To change this template use File | Settings | File Templates.
  */
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employeeId")
+    private long employeeId;
+
+    @Column(name = "employeeName")
+    private String employeeName;
+
+    @Column(name = "employeeRole")
+    private String employeeRole;
+
+
 }
