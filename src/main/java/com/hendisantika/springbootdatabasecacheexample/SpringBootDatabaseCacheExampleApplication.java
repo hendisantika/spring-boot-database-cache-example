dc332f33fed74e065db22289aee28565b11fbfc8
Hendi Santika <hendisantika@gmail.com>
2018-09-26 06:47:51 +0700

    #4 - Add @EnableCaching annotation to enable spring cache database

--- src/main/java/com/hendisantika/springbootdatabasecacheexample/SpringBootDatabaseCacheExampleApplication.java
+++ src/main/java/com/hendisantika/springbootdatabasecacheexample/SpringBootDatabaseCacheExampleApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.springbootdatabasecacheexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching      // to enable spring cache
 public class SpringBootDatabaseCacheExampleApplication {
 
     public static void main(String[] args) {
