a92c9e2340ba3c2b6e4fb8931ad2d9688578835e
Hendi Santika <hendisantika@gmail.com>
2018-09-26 06:49:12 +0700

    #6 - Add lombok lib

---
+++ src/main/java/com/hendisantika/springbootdatabasecacheexample/entity/Employee.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootdatabasecacheexample.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-database-cache-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/09/18
+ * Time: 06.48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+}
